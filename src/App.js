79dc65f3271d056e21286014a76abad7fb33e81c
Tom Jamieson <tomjamieson09@gmail.com>
2025-04-09 14:05:03 +1000

    Test minimal App.js to isolate React error #130

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
-// App.js content for BuildCraft Beta Drop
+function App() {
+  return (
+    <div style={{ padding: 40, fontSize: 28 }}>
+      BuildCraft is loading successfully!
+    </div>
+  );
+}
+
+export default App;
