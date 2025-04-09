14dad84384cd4d5a6a3272b7c329659fcb2901f6
Tom Jamieson <tomjamieson09@gmail.com>
2025-04-09 17:38:14 +1000

    Reset: Stable base login setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,12 @@
-// Login component with Guest login
+
+import React from 'react';
+
+function Login() {
+  return (
+    <div style={{ padding: 40, fontSize: 24 }}>
+      🧪 Login Component Rendered Successfully
+    </div>
+  );
+}
+
+export default Login;
